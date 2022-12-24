ecd249dee26fc8143011153f82e5bbb76c33f342
GitHub Action <action@github.com>
2022-12-24 03:58:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/c2ray/pojoutil/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/c2ray/pojoutil/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.c2ray.pojoutil.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.c2ray.pojoutil.MyBundle
 
 class MyApplicationService {
 
